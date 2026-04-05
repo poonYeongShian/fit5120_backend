8a5e63402e6699393e31e27bdd569004bbe4561a
Yeong Shian Poon <yeongshian.poon@timeteccloud.com>
2026-04-06 00:47:27 +0800

    feature add filter by category for animal list -add id

--- Deploy/DTOs/AnimalCardDto.cs
+++ Deploy/DTOs/AnimalCardDto.cs
@@ -2,6 +2,7 @@ namespace Deploy.DTOs;
 
 public class AnimalCardDto
 {
+    public int Id { get; set; }
     public string CommonName { get; set; } = string.Empty;
     public string ScientificName { get; set; } = string.Empty;
     public string Category { get; set; } = string.Empty;
